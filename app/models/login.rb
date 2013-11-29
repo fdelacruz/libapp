7a145dcd415ee373684d0dc6ed5b1a3baef28277
Francisco De La Cruz <helloquico@gmail.com>
2013-11-28 19:17:48 -0500

    Login feature happy path

--- app/models/login.rb
+++ app/models/login.rb
@@ -2,5 +2,14 @@ class Login
   include ActiveAttr::BasicModel
   include ActiveAttr::MassAssignment
   attr_accessor :email, :password
+
+  def authenticate
+    reader = Reader.find_by_email(email)
+    if reader && reader.authenticate(password)
+      reader.id
+    else
+      nil
+    end
+  end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131122201648) do
+ActiveRecord::Schema.define(:version => 20131128224603) do
 
   create_table "readers", :force => true do |t|
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
